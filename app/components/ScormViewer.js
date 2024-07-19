6688253839179421eefb519ce4af9c85899f8a19
himmatkumar9 <himmat.k@sarasai.org>
2024-07-19 11:22:52 +0530

    path changed

--- app/components/ScormViewer.js
+++ app/components/ScormViewer.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import  './scormfunctions';
+// import  './scormfunctions';
 
 const ScormViewer = ({  }) => {
   const [scormUrl, setScormUrl] = useState('');
