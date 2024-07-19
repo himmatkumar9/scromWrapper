957c1c44c67654ccaffcfc1b856d92e67d0e3c4a
himmatkumar9 <himmat.k@sarasai.org>
2024-07-19 10:56:48 +0530

    path changed

--- app/components/ScormViewer.js
+++ app/components/ScormViewer.js
@@ -50,7 +50,7 @@ const ScormViewer = ({  }) => {
     <div>
       <iframe  
       crossOrigin = 'true'
-      src={'https://lti-backend.vercel.app/scrom/imsmanifest.xml'} title="SCORM Package" width="100%" height="600px"></iframe>
+      src={'https://lti-backend.vercel.app/scrom/scormcontent/index.html'} title="SCORM Package" width="100%" height="600px"></iframe>
     </div>
   );
 };
