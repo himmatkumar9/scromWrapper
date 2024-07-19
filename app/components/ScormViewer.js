c39bab9b4247cab8f51a97b821b45544cef48ba9
himmatkumar9 <himmat.k@sarasai.org>
2024-07-19 12:46:03 +0530

    path changed

--- app/components/ScormViewer.js
+++ app/components/ScormViewer.js
@@ -1,62 +1,41 @@
-"use client";
-
 import React, { useEffect, useState } from 'react';
-// import  './scormfunctions';
 
-const ScormViewer = ({  }) => {
-  const [scormUrl, setScormUrl] = useState('');
+const ScormViewer = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const [apiReady, setApiReady] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
 
-  useEffect(()=>{
+    window.addEventListener('message', (event) => {
+      if (event.origin !== 'https://www.nailsbyru.com') {
+        return;
+      }
+      console.log('Message from iframe:', event.data);
+    });
+  }, []);
 
-  setTimeout(() => {
-    setLoading(false)
-  }, 3000);
-  },[])
-
-
-  // useEffect(() => {
-  //   const fetchScormUrl = async () => {
-  //     try {
-  //       const response = await fetch(`https://ltibackend.onrender.com/scrom/`);
-  //       if (!response.ok) {
-  //         throw new Error('Failed to fetch SCORM content');
-  //       }
-  //       const blob = await response.blob();
-  //       const url = URL.createObjectURL(blob);
-  //       console.log(url)
-  //       setScormUrl(url);
-  //       setLoading(false);
-  //     } catch (error) {
-  //       setError(error.message || 'An error occurred');
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchScormUrl();
-  // }, []);
-
-  // if (!apiReady) {
-  //   return <div>Loading SCORM API...</div>;
-  // }
-  // if (error) {
-  //   return <div>Error: {error}</div>;
-  // }
+  const handleIframeLoad = () => {
+    const iframe = document.getElementById('scormIframe');
+    iframe.contentWindow.postMessage('Hello from parent', 'https://www.nailsbyru.com');
+  };
 
   return (
     <div>
-      
-  <iframe  
-      // sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-      crossOrigin = 'true'
-      src={'https://www.nailsbyru.com/scrom/topic-5/scormcontent/index.html'} title="SCORM Package" width="100%" height="600px"></iframe>
-      {/* <iframe  
-      sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-      crossOrigin = 'true'
-      src={'https://lti-backend.vercel.app/scrom/topic-5/scormcontent/index.html'} title="SCORM Package" width="100%" height="600px"></iframe> */}
+      {loading ? <div>Loading SCORM API...</div> : (
+        <iframe  
+          id="scormIframe"
+          src='https://www.nailsbyru.com/scrom/topic-5/scormcontent/index.html'
+          title="SCORM Package"
+          width="100%"
+          height="600px"
+          onLoad={handleIframeLoad}
+          sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+        ></iframe>
+      )}
+      {error && <div>Error: {error}</div>}
     </div>
   );
 };
