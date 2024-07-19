385892a0df32fbc537d414cd7c3bb32bcd1625ee
himmatkumar9 <himmat.k@sarasai.org>
2024-07-19 11:45:40 +0530

    path changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
-
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "frame-src 'self' https://lti-backend.vercel.app",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
 export default nextConfig;
